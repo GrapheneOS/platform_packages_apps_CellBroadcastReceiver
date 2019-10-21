92c34946057adecc5341df7bdc967daba33877c3
Jack Yu <jackyu@google.com>
2019-10-21 10:59:16 -0700

    Enabled geo-fencing trigger message
    Enabled the geo-fencing trigger messages
    for the United States.
    
    Bug: 142670652
    Test: Manual
    Merged-In: Ifee9eec0fb91d107bcdda8aeda475194dea943d4
    Change-Id: Ifee9eec0fb91d107bcdda8aeda475194dea943d4

--- src/com/android/cellbroadcastreceiver/CellBroadcastConfigService.java
+++ src/com/android/cellbroadcastreceiver/CellBroadcastConfigService.java
@@ -164,6 +164,8 @@ public class CellBroadcastConfigService extends IntentService {
         boolean enableEmergencyAlerts = enableAlertsMasterToggle && prefs.getBoolean(
                 CellBroadcastSettings.KEY_ENABLE_EMERGENCY_ALERTS, true);
 
+        boolean enableGeoFencingTriggerMessage = enableForSub;
+
         if (VDBG) {
             log("enableAlertsMasterToggle = " + enableAlertsMasterToggle);
             log("enableEtwsAlerts = " + enableEtwsAlerts);
@@ -177,6 +179,7 @@ public class CellBroadcastConfigService extends IntentService {
                     + enablePublicSafetyMessagesChannelAlerts);
             log("enableStateLocalTestAlerts = " + enableStateLocalTestAlerts);
             log("enableEmergencyAlerts = " + enableEmergencyAlerts);
+            log("enableGeoFencingTriggerMessage = " + enableGeoFencingTriggerMessage);
         }
 
         /** Enable CMAS series messages. */
@@ -231,6 +234,11 @@ public class CellBroadcastConfigService extends IntentService {
         setCellBroadcastRange(manager, enableStateLocalTestAlerts,
                 CellBroadcastChannelManager.getInstance().getCellBroadcastChannelRanges(this,
                         R.array.state_local_test_alert_range_strings));
+
+        // Enable/Disable GSM geo-fencing trigger messages.
+        setCellBroadcastRange(manager, enableStateLocalTestAlerts,
+                CellBroadcastChannelManager.getInstance().getCellBroadcastChannelRanges(this,
+                        R.array.geo_fencing_trigger_messages_range_strings));
 
         /** Enable non-CMAS series messages. */
 
