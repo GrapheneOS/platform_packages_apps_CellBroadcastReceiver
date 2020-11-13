db5597e989674e114962f973ce0ba0f2dd776739
Chen Xu <fionaxu@google.com>
2020-11-12 20:30:20 -0800

    DO NOT MERGE reenable navigation bar when app is not visible
    Bug: 171614598
    Test: Manual test emergency alert + incoming call
    Change-Id: I3480b860bb840dee58dde0dad5631cb8a7530047

--- src/com/android/cellbroadcastreceiver/CellBroadcastAlertDialog.java
+++ src/com/android/cellbroadcastreceiver/CellBroadcastAlertDialog.java
@@ -325,11 +325,6 @@ public class CellBroadcastAlertDialog extends Activity {
                 | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-        // Some LATAM carriers mandate to disable navigation bars, quick settings etc when alert
-        // dialog is showing. This is to make sure users to ack the alert before switching to
-        // other activities.
-        setStatusBarDisabledIfNeeded(true);
-
         // Initialize the view.
         LayoutInflater inflater = LayoutInflater.from(this);
         setContentView(inflater.inflate(R.layout.cell_broadcast_alert, null));
@@ -419,6 +414,10 @@ public class CellBroadcastAlertDialog extends Activity {
                 mAnimationHandler.startIconAnimation(subId);
             }
         }
+        // Some LATAM carriers mandate to disable navigation bars, quick settings etc when alert
+        // dialog is showing. This is to make sure users to ack the alert before switching to
+        // other activities.
+        setStatusBarDisabledIfNeeded(true);
     }
 
     /**
@@ -429,6 +428,7 @@ public class CellBroadcastAlertDialog extends Activity {
     public void onPause() {
         Log.d(TAG, "onPause called");
         mAnimationHandler.stopIconAnimation();
+        setStatusBarDisabledIfNeeded(false);
         super.onPause();
     }
 
@@ -892,7 +892,6 @@ public class CellBroadcastAlertDialog extends Activity {
 
     @Override
     public void onDestroy() {
-        setStatusBarDisabledIfNeeded(false);
         super.onDestroy();
     }
 
