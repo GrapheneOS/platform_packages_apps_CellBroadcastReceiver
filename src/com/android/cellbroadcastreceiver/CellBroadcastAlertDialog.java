0af73df51f2607278b8ecca32e25260dc1b93cca
Chen Xu <fionaxu@google.com>
2021-02-05 11:30:06 +0800

    improve dialog touch outside logic
    Finish alert dialog only if all messages are configured with
    DismissOnOutsideTouch. When multiple messages are displayed,
    the message with dismissOnOutsideTouch(normally low priority message)
    is displayed on top of other unread alerts without dismissOnOutsideTouch,
    users can easily dismiss all messages by touching the screen. better way
    is to dismiss the alert if and only if all messages with
    dismiss_on_outside_touch set true.
    
    Bug: 173837273
    Test: Manual test
    Change-Id: Id51a15e4c904cffb7c277cd0569de5016d87ea0f
    Merged-in: I62b930c31c3f20575f15c653533d15816c00c184

--- src/com/android/cellbroadcastreceiver/CellBroadcastAlertDialog.java
+++ src/com/android/cellbroadcastreceiver/CellBroadcastAlertDialog.java
@@ -409,9 +409,7 @@ public class CellBroadcastAlertDialog extends Activity {
                 mScreenOffHandler.startScreenOnTimer(message);
             }
 
-            CellBroadcastChannelRange range =
-                    channelManager.getCellBroadcastChannelRangeFromMessage(message);
-            setFinishOnTouchOutside(range != null && range.mDismissOnOutsideTouch);
+            setFinishAlertOnTouchOutside();
 
             updateAlertText(message);
 
@@ -910,6 +908,7 @@ public class CellBroadcastAlertDialog extends Activity {
             }
 
             hideOptOutDialog(); // Hide opt-out dialog when new alert coming
+            setFinishAlertOnTouchOutside();
             updateAlertText(getLatestMessage());
             // If the new intent was sent from a notification, dismiss it.
             clearNotification(intent);
@@ -1004,6 +1003,7 @@ public class CellBroadcastAlertDialog extends Activity {
         // If there are older emergency alerts to display, update the alert text and return.
         SmsCbMessage nextMessage = getLatestMessage();
         if (nextMessage != null) {
+            setFinishAlertOnTouchOutside();
             updateAlertText(nextMessage);
             int subId = nextMessage.getSubscriptionId();
             if (channelManager.isEmergencyMessage(nextMessage)
@@ -1145,6 +1145,29 @@ public class CellBroadcastAlertDialog extends Activity {
            CellBroadcastAlertService.addToNotificationBar(
                    CellBroadcastReceiverApp.getLatestMessage(),
                    unreadMessageList, context,false, false, false);
+        }
+    }
+
+    /**
+     * Finish alert dialog only if all messages are configured with DismissOnOutsideTouch.
+     * When multiple messages are displayed, the message with dismissOnOutsideTouch(normally low
+     * priority message) is displayed on top of other unread alerts without dismissOnOutsideTouch,
+     * users can easily dismiss all messages by touching the screen. better way is to dismiss the
+     * alert if and only if all messages with dismiss_on_outside_touch set true.
+     */
+    private void setFinishAlertOnTouchOutside() {
+        if (mMessageList != null) {
+            int dismissCount = 0;
+            for (SmsCbMessage message : mMessageList) {
+                CellBroadcastChannelManager channelManager = new CellBroadcastChannelManager(
+                        this, message.getSubscriptionId());
+                CellBroadcastChannelManager.CellBroadcastChannelRange range =
+                        channelManager.getCellBroadcastChannelRangeFromMessage(message);
+                if (range != null && range.mDismissOnOutsideTouch) {
+                    dismissCount++;
+                }
+            }
+            setFinishOnTouchOutside(mMessageList.size() > 0 && mMessageList.size() == dismissCount);
         }
     }
 
