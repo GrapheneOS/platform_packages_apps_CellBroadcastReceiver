d09281b46af89d173d00747d36e5e961b9fc95c1
Hui Wang <huiwang@google.com>
2021-08-03 22:49:05 +0000

    Support to enable channels on debug build only
    Add new resource overlay to enable channels on debug build only, and
    update config for UK test channels
    
    Bug: 195541960
    Bug: 193204744
    Bug: 194279546
    Test: atest CellBroadcastReceiverOemUnitTests:CellBroadcastChannelManagerTest
    Test: manual
    Change-Id: Id07983bbb66f4379d4e6ab8b30f8df264ae0478b

--- src/com/android/cellbroadcastreceiver/CellBroadcastReceiver.java
+++ src/com/android/cellbroadcastreceiver/CellBroadcastReceiver.java
@@ -174,17 +174,14 @@ public class CellBroadcastReceiver extends BroadcastReceiver {
             // rename registered notification channels on locale change
             CellBroadcastAlertService.createNotificationChannels(mContext);
         } else if (TelephonyManager.ACTION_SECRET_CODE.equals(action)) {
-            if (SystemProperties.getInt("ro.debuggable", 0) == 1
-                    || res.getBoolean(R.bool.allow_testing_mode_on_user_build)) {
-                setTestingMode(!isTestingMode(mContext));
-                int msgId = (isTestingMode(mContext)) ? R.string.testing_mode_enabled
-                        : R.string.testing_mode_disabled;
-                String msg =  res.getString(msgId);
-                Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
-                LocalBroadcastManager.getInstance(mContext)
-                        .sendBroadcast(new Intent(ACTION_TESTING_MODE_CHANGED));
-                log(msg);
-            }
+            setTestingMode(!isTestingMode(mContext));
+            int msgId = (isTestingMode(mContext)) ? R.string.testing_mode_enabled
+                    : R.string.testing_mode_disabled;
+            String msg =  res.getString(msgId);
+            Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+            LocalBroadcastManager.getInstance(mContext)
+                    .sendBroadcast(new Intent(ACTION_TESTING_MODE_CHANGED));
+            log(msg);
         } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
             new CellBroadcastContentProvider.AsyncCellBroadcastTask(
                     mContext.getContentResolver()).execute((CellBroadcastContentProvider
