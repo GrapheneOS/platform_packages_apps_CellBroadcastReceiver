85c4344afcce01e3de414b108068fa84a95eee67
Hyein Yu <hyeinyu@google.com>
2021-10-23 11:54:50 +0000

    Additional Fix about mismatch of unread count when"Show full-screen messages" is OFF
    This is additional fix for ag/15958392.
    ag/15958392 makes notification with empty list when onStop is called after user clicks the notification.
    In this commit, b/201597124 is fixed again.
    
    Bug: 201597124
    Bug: 203778554
    Test: Manual
    Test: atest com.android.cellbroadcastreceiver.unit.CellBroadcastAlertDialogTest
    Change-Id: I9807a45a9bd284c44137f3d7638c1584d5725d83

--- tests/unit/src/com/android/cellbroadcastreceiver/unit/CellBroadcastAlertDialogTest.java
+++ tests/unit/src/com/android/cellbroadcastreceiver/unit/CellBroadcastAlertDialogTest.java
@@ -28,6 +28,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.IPowerManager;
 import android.os.IThermalService;
@@ -44,7 +45,6 @@ import android.widget.TextView;
 
 import com.android.cellbroadcastreceiver.CellBroadcastAlertDialog;
 import com.android.cellbroadcastreceiver.CellBroadcastAlertService;
-import com.android.cellbroadcastreceiver.CellBroadcastSettings;
 import com.android.cellbroadcastreceiver.R;
 import com.android.internal.telephony.gsm.SmsCbConstants;
 
@@ -89,6 +89,9 @@ public class CellBroadcastAlertDialogTest extends
     private int mCmasMessageClass = 0;
 
     private ArrayList<SmsCbMessage> mMessageList;
+
+    @Mock
+    Resources mMockResources;
 
     @Override
     protected Intent createActivityIntent() {
@@ -177,6 +180,48 @@ public class CellBroadcastAlertDialogTest extends
                 b.getCharSequence(Notification.EXTRA_TITLE).toString()));
         assertEquals(CellBroadcastAlertServiceTest.createMessage(98235).getMessageBody(),
                 b.getCharSequence(Notification.EXTRA_TEXT));
+    }
+
+    public void testGetNewMessageListIfNeeded() throws Throwable {
+        CellBroadcastAlertDialog activity = startActivity();
+        mContext.setResources(mMockResources);
+        doReturn(false).when(mMockResources).getBoolean(
+                R.bool.show_cmas_messages_in_priority_order);
+        SmsCbMessage testMessage1 = CellBroadcastAlertServiceTest
+                .createMessageForCmasMessageClass(12412,
+                        SmsCbConstants.MESSAGE_ID_CMAS_ALERT_PRESIDENTIAL_LEVEL,
+                        mCmasMessageClass);
+        waitForMs(10);
+        SmsCbMessage testMessage2 = CellBroadcastAlertServiceTest
+                .createMessageForCmasMessageClass(12412,
+                        SmsCbConstants.MESSAGE_ID_CMAS_ALERT_CHILD_ABDUCTION_EMERGENCY,
+                        mCmasMessageClass);
+        ArrayList<SmsCbMessage> inputList1 = new ArrayList<>();
+        ArrayList<SmsCbMessage> inputList2 = new ArrayList<>();
+
+        inputList1.add(testMessage1);
+        ArrayList<SmsCbMessage> messageList = activity.getNewMessageListIfNeeded(
+                inputList1, inputList2);
+        assertTrue(messageList.size() == 1);
+        assertEquals(testMessage1.getReceivedTime(), messageList.get(0).getReceivedTime());
+
+        inputList2.add(testMessage1);
+        messageList = activity.getNewMessageListIfNeeded(inputList1, inputList2);
+        assertTrue(messageList.size() == 1);
+        assertEquals(testMessage1.getReceivedTime(), messageList.get(0).getReceivedTime());
+
+        inputList2.add(testMessage2);
+        messageList = activity.getNewMessageListIfNeeded(inputList1, inputList2);
+        assertTrue(messageList.size() == 2);
+        assertEquals(testMessage2.getReceivedTime(), messageList.get(1).getReceivedTime());
+
+        doReturn(true).when(mMockResources).getBoolean(
+                R.bool.show_cmas_messages_in_priority_order);
+        messageList = activity.getNewMessageListIfNeeded(inputList1, inputList2);
+        assertTrue(messageList.size() == 2);
+        assertEquals(testMessage1.getReceivedTime(), messageList.get(1).getReceivedTime());
+
+        mContext.setResources(null);
     }
 
     @InstrumentationTest
