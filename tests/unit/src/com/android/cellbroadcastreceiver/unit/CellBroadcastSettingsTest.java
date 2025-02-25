d73af1a75d7da492d2a61437068f0c4685bb415d
Adrian Roos <roosa@google.com>
2025-02-25 15:37:01 +0100

    packages/apps/CellBroadcastReceiver: remove deprecated Mockito usages
    Test: m checkbuild
    Flag: TEST_ONLY
    Bug: 310268946
    Change-Id: Icaf5f09913292d86db2a3e0fc8457aad4cff740a

--- tests/unit/src/com/android/cellbroadcastreceiver/unit/CellBroadcastSettingsTest.java
+++ tests/unit/src/com/android/cellbroadcastreceiver/unit/CellBroadcastSettingsTest.java
@@ -21,9 +21,9 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
